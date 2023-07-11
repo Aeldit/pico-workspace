dce19a9e3f04a90eb65782f94f293e7b7b6a668a
Aeldit <raphael.roger.92@gmail.com>
2023-07-11 23:01:37 +0200

    t

--- test_1/test_1.cpp
+++ test_1/test_1.cpp
@@ -16,19 +16,19 @@
                         GP2  | 4            37 | 3V3_EN
                         GP3  | 5            36 | 3V3_OUT
                         GP4  | 6            35 | ADC_VREF
-                        GP5  | 7            34 | GP28
+                        GP5  | 7            34 | GP28    BTN_LCD_ENTER
                         GND <| 8            33 |>GND
-                        GP6  | 9            32 | GP27
-                        GP7  | 10  +-----+  31 | GP26
+                        GP6  | 9            32 | GP27    BTN_LCD_DOWN
+                        GP7  | 10  +-----+  31 | GP26    BTN_LCD_UP
                         GP8  | 11  |     |  30 | RUN
-                        GP9  | 12  |     |  29 | GP22
+                        GP9  | 12  |     |  29 | GP22    BTN_LCD_CANCEL
                         GND <| 13  +-----+  28 |>GND
                         GP10 | 14           27 | GP21
                         GP11 | 15           26 | GP20
                         GP12 | 16           25 | GP19
                         GP13 | 17           24 | GP18
                         GND <| 18           23 |>GND
-            DISP_SDA    GP14 | 19           22 | GP17    BTN_LCD_ENTER
+            DISP_SDA    GP14 | 19           22 | GP17
             DISP_SCL    GP15 | 20           21 | GP16    BTN_LIFE
                              +-----------------+
 */
@@ -52,31 +52,39 @@
 #define SERIAL_DEBUG
 
 #define NB_LEDS 1
-#define NB_BUTTONS 2
+#define NB_BUTTONS 5
 
 // PINs
 //==============================
-#define DISP_SDA_PIN 14
-#define DISP_SCL_PIN 15
+#define PIN_DISP_SDA 14
+#define PN_DISP_SCL 15
 
-#define PIN_LIFE_BTN 16
-#define PIN_BTN_LCD_ENTER 17
+#define PIN_BTN_LIFE 16
+
+#define PIN_BTN_LCD_CANCEL 22
 
 #define PIN_LIFE_LED 25
 
+#define PIN_BTN_LCD_UP 26
+#define PIN_BTN_LCD_DOWN 27
+#define PIN_BTN_LCD_ENTER 28
+
 // Indexes
 //==============================
-#define LIFE_LED 0
+#define LED_LIFE 0
 
 #define BTN_LIFE 0
-#define BTN_LCD_ENTER 1
+#define BTN_LCD_CANCEL 1
+#define BTN_LCD_UP 2
+#define BTN_LCD_DOWN 3
+#define BTN_LCD_ENTER 4
 
 // Tempos
 //==============================
 #define C_TIME_BUTTON_FILTER 30000     // 30ms : button filtering time
 #define C_TIME_LIFE_LED_FILTER 1000000 // 1s : life LED filtering time
 
-#define C_TIME_IS_LCD_ON 10000000        // 10s : duration before putting display asleep
+#define C_TIME_LCD_SLEEP 4000000         // 10s : duration before putting display asleep
 #define C_TIME_LCD_BUTTONS_FILTER 250000 // 250ms : LEDs filtering time
 #define C_TIME_LCD_SELECTOR 300000       // 300ms : duration before putting display asleep
 
@@ -85,22 +93,25 @@
 //=============================================================
 // CONSTANTS
 // ============================================================
-const uint8_t BUTTONS_PINS[NB_BUTTONS] = {PIN_LIFE_BTN, PIN_BTN_LCD_ENTER}; // Buttons PINs order for LEDs:
-const uint8_t LEDS_PINS[NB_LEDS] = {PIN_LIFE_LED};                          // LEDs PINs order for LEDs:
+const uint8_t BUTTONS_PINS[NB_BUTTONS] = {
+    PIN_BTN_LIFE,
+    PIN_BTN_LCD_CANCEL, PIN_BTN_LCD_UP, PIN_BTN_LCD_DOWN, PIN_BTN_LCD_ENTER}; // Buttons PINs
+const uint8_t LEDS_PINS[NB_LEDS] = {PIN_LIFE_LED};                            // LEDs PINs
 
 //=============================================================
 // VARIABLES
 // ============================================================
-bool buttons_states[NB_BUTTONS] = {false, false};          // Buttons order:
-bool previous_buttons_states[NB_BUTTONS] = {false, false}; // Same as above
-bool leds_states[NB_LEDS] = {true};                        // LEDs order:
+bool buttons_states[NB_BUTTONS] = {false, false, false, false, false};          // Buttons order:
+bool previous_buttons_states[NB_BUTTONS] = {false, false, false, false, false}; // Same as above
+bool leds_states[NB_LEDS]{true};                                                // LEDs order:
 
-uint8_t buttons_counters[NB_BUTTONS] = {0, 0}; // Counters used for buttons acquisition
+uint8_t buttons_counters[NB_BUTTONS] = {0, 0, 0, 0, 0}; // Counters used for buttons acquisition
 
-absolute_time_t timer_button;     // Timer for buttons acquisition
-absolute_time_t timer_life_led;   // Timer for life led blinking
-absolute_time_t timer_lcd;        // Timer for the LCD to shutdown
-absolute_time_t timer_lcd_button; // Timer for the LCD button
+uint8_t pressed_button = NB_BUTTONS;
+
+absolute_time_t timer_button;   // Timer for buttons acquisition
+absolute_time_t timer_life_led; // Timer for life led blinking
+absolute_time_t timer_lcd;      // Timer for the LCD to shutdown
 
 // Events
 //==============================
@@ -157,22 +168,31 @@ int main()
 
     // Buttons
     //==============================
-    gpio_init(PIN_LIFE_BTN);
+    gpio_init(PIN_BTN_LIFE);
+    gpio_init(PIN_BTN_LCD_CANCEL);
+    gpio_init(PIN_BTN_LCD_UP);
+    gpio_init(PIN_BTN_LCD_DOWN);
     gpio_init(PIN_BTN_LCD_ENTER);
 
-    gpio_set_dir(PIN_LIFE_BTN, GPIO_IN);
+    gpio_set_dir(PIN_BTN_LIFE, GPIO_IN);
+    gpio_set_dir(PIN_BTN_LCD_CANCEL, GPIO_IN);
+    gpio_set_dir(PIN_BTN_LCD_UP, GPIO_IN);
+    gpio_set_dir(PIN_BTN_LCD_DOWN, GPIO_IN);
     gpio_set_dir(PIN_BTN_LCD_ENTER, GPIO_IN);
 
-    gpio_set_pulls(PIN_LIFE_BTN, true, false);
+    gpio_set_pulls(PIN_BTN_LIFE, true, false);
+    gpio_set_pulls(PIN_BTN_LCD_CANCEL, true, false);
+    gpio_set_pulls(PIN_BTN_LCD_UP, true, false);
+    gpio_set_pulls(PIN_BTN_LCD_DOWN, true, false);
     gpio_set_pulls(PIN_BTN_LCD_ENTER, true, false);
 
     // LCD
     //==============================
     i2c_init(i2c1, 400000);                         // Initialize I2C on i2c0 port with 400kHz
-    gpio_set_function(DISP_SDA_PIN, GPIO_FUNC_I2C); // Use DISP_SDA_PIN as I2C
-    gpio_set_function(DISP_SCL_PIN, GPIO_FUNC_I2C); // Use DISP_SCL_PIN as I2C
-    gpio_pull_up(DISP_SDA_PIN);                     // Pull up DISP_SDA_PIN
-    gpio_pull_up(DISP_SCL_PIN);                     // Pull up DISP_SCL_PIN
+    gpio_set_function(PIN_DISP_SDA, GPIO_FUNC_I2C); // Use PIN_DISP_SDA as I2C
+    gpio_set_function(PN_DISP_SCL, GPIO_FUNC_I2C);  // Use PN_DISP_SCL as I2C
+    gpio_pull_up(PIN_DISP_SDA);                     // Pull up PIN_DISP_SDA
+    gpio_pull_up(PN_DISP_SCL);                      // Pull up PN_DISP_SCL
     //  LCD Init
     oled = new GFX(0x3C, size::W128xH64, i2c1); // Declare oled instance
     oled->display(logo);
@@ -185,42 +205,16 @@ int main()
     //=======================================================
     while (true)
     {
-        // Life LED tempo (blinking the white LED)
-        /*if (option_debug_life_led)
-        {
-            if (absolute_time_diff_us(timer_life_led, get_absolute_time()) > C_TIME_LIFE_LED_FILTER)
-            {
-                led_driving(LIFE_LED);
-                timer_life_led = get_absolute_time();
-            }
-        }
-        else // Shuts down the led if it was on
-        {
-            if (leds_states[LIFE_LED])
-            {
-                leds_states[LIFE_LED] = false;
-                led_driving(LIFE_LED);
-            }
-        }*/
-
         // Buttons
         buttons_acquisition();
 
-        if (event_button_updated)
+        if (event_button_updated && pressed_button != NB_BUTTONS)
         {
-            event_button_updated = false;
-
-#ifdef SERIAL_DEBUG
-            printf("Button pressed");
-#endif
-            if (buttons_states[BTN_LIFE])
-            {
-                on_button_release(BTN_LIFE);
-            }
+            on_button_release(pressed_button);
         }
 
         // LCD
-        // display_management();
+        display_management();
     }
 }
 
@@ -259,6 +253,15 @@ void button_acquisition(uint8_t button)
         {
             previous_buttons_states[button] = buttons_states[button];
             event_button_updated = true;
+
+            if (buttons_states[button])
+            {
+                pressed_button = button;
+            }
+            else
+            {
+                pressed_button = NB_BUTTONS;
+            }
         }
     }
 }
@@ -276,9 +279,17 @@ void on_button_release(uint8_t button)
     switch (button)
     {
     case BTN_LIFE:
-        led_driving(LIFE_LED);
+        led_driving(LED_LIFE);
+        break;
+
+    case BTN_LCD_ENTER:
+        is_display_update_needed = true;
+        timer_lcd = get_absolute_time();
         break;
     }
+
+    event_button_updated = false;
+    pressed_button = NB_BUTTONS;
 }
 
 // LEDs
@@ -309,57 +320,30 @@ void leds_driving()
 //=======================================================
 void display_management()
 {
-    // TODO -> make work Turns on the LCD if a button is pressed (and if the LCD was turned off)
-    /*if (!is_lcd_on)
-    {
-        if (buttons_state[LCD_DOWN_BTN] || buttons_state[LCD_UP_BTN] || buttons_state[LCD_LEFT_BTN] || buttons_state[LCD_RIGHT_BTN] || buttons_state[LCD_ENTER_BTN])
-        {
-            is_display_update_needed = true;
-            timer_lcd = get_absolute_time();
-        }
-    }*/
-
-    /*if (buttons_states[BTN_LCD_ENTER])
-    {
-        if (absolute_time_diff_us(timer_lcd_button, get_absolute_time()) > C_TIME_LCD_BUTTONS_FILTER)
-        {
-            if (is_lcd_on)
-            {
-                on_button_press(BTN_LCD_ENTER);
-                timer_lcd_button = get_absolute_time();
-            }
-            else
-            {
-                is_display_update_needed = true;
-                timer_lcd_button = timer_lcd = get_absolute_time();
-            }
-        }
-    }*/
-
     if (is_display_update_needed && !is_lcd_on)
     {
         is_lcd_on = true;
+        oled->display();
     }
 
     if (is_lcd_on)
     {
-        oled->clear();
-        oled->display();
         // Put display asleep after timeout
-        /*if (absolute_time_diff_us(timer_lcd, get_absolute_time()) > C_TIME_IS_LCD_ON)
+        if (absolute_time_diff_us(timer_lcd, get_absolute_time()) > C_TIME_LCD_SLEEP)
         {
             is_lcd_on = false;
-            is_display_update_needed = false;
             oled->clear();
             oled->display();
         }
-
-        if (is_display_update_needed)
+        else
         {
-            char option[15] = "Screen Timeout";
-            char Text[1];
-            screen_option_toggle(oled, option, option_debug_life_led, selected_toggle);
-            is_display_update_needed = false;
-        }*/
+            if (is_display_update_needed)
+            {
+                char option[15] = "Screen Test";
+                char Text[1];
+                screen_option_toggle(oled, option, option_debug_life_led, selected_toggle);
+                is_display_update_needed = false;
+            }
+        }
     }
 }
